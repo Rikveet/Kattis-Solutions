51d96398a71dfe90cc8c2796389fd79595d78839
Capt_KillSwitch <rh18vo@gmail.com>
2022-10-21 14:42:11 -0400

    - deleted unnecessary files. - fixed a bug in half a cookie.

--- half a cookie/main.cpp
+++ half a cookie/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#define _USE_MATH_DEFINES
 #include <cmath>
 using namespace std;
-int main() {
-    double r, x, y,rValid,totalarea,area;
-    cin>> r>> x >>y ;
-    rValid = sqrt(pow(x,2)+pow(y,2));
-    if (rValid>r){
-        cout <<" miss";
+int main()
+{
+    double r, x, y, rValid, totalarea, area;
+    cin >> r >> x >> y;
+    rValid = sqrt(pow(x, 2) + pow(y, 2));
+    if (rValid > r)
+    {
+        cout << " miss";
         return 0;
     }
-    totalarea = M_PI*r*r;
-    r = r-rValid;
-    area = M_PI*r*r/2;
-    cout << totalarea - area << " "<< area;
+    totalarea = M_PI * r * r;
+    r = r - rValid;
+    area = M_PI * r * r / 2;
+    cout << totalarea - area << " " << area;
     return 0;
 }
